e97332ce1bbfbb1d0888aab5b50e06b62629c931
Leonardo César <leo_cesa@hotmail.com>
2024-01-16 17:02:23 -0300

    fix: filter in listTransactionReportBy

--- src/server/models/TransactionReport/read/listTransactionReportBy.ts
+++ src/server/models/TransactionReport/read/listTransactionReportBy.ts
@@ -5,46 +5,46 @@ import { debugDev } from "@/utils/dev";
 import { TransactionReport } from "../schema";
 
 interface IListTransactionReport {
-    bankAccountId: string;
-    type?: TransactionReport["type"];
-    filters?: FirebaseFilterFor<TransactionReport>[];
+  bankAccountId: string;
+  type?: TransactionReport["type"];
+  filters?: FirebaseFilterFor<TransactionReport>[];
+}
+export const listTransactionReportsBy = async ({
+  bankAccountId,
+  type,
+  filters = [],
+}: IListTransactionReport): Promise<AppModelResponse<TransactionReport[]>> => {
+  const funcName = "listTransactionReportsBy";
+
+  if (type && !filters.find((filter) => filter.field === "type")) {
+    filters.push({ field: "type", operator: "==", value: type });
   }
-  export const listTransactionReportsBy = async ({
-    bankAccountId,
-    type,
-    filters = [],
-  }: IListTransactionReport): Promise<AppModelResponse<TransactionReport[]>> => {
-    const funcName = "listTransactionReportsBy";
-  
-    if (type) {
-      filters.push({ field: "type", operator: "==", value: type });
-    }
-  
-    try {
-      const list = await firebaseList<TransactionReport>({
-        collection: FirebaseCollection.transactionReports,
-        filters: [
-          { field: "bankAccountId", operator: "==", value: bankAccountId },
-          ...filters,
-        ],
-      });
-      return {
-        data: list,
-        done: true,
-        error: null,
-      };
-    } catch (error) {
-      const errorMessage = debugDev({
-        type: "error",
-        name: funcName,
-        value: error,
-      });
-      return {
-        data: null,
-        done: false,
-        error: {
-          message: errorMessage,
-        },
-      };
-    }
-  };
+
+  try {
+    const list = await firebaseList<TransactionReport>({
+      collection: FirebaseCollection.transactionReports,
+      filters: [
+        { field: "bankAccountId", operator: "==", value: bankAccountId },
+        ...filters,
+      ],
+    });
+    return {
+      data: list,
+      done: true,
+      error: null,
+    };
+  } catch (error) {
+    const errorMessage = debugDev({
+      type: "error",
+      name: funcName,
+      value: error,
+    });
+    return {
+      data: null,
+      done: false,
+      error: {
+        message: errorMessage,
+      },
+    };
+  }
+};
