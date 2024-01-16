6fa4b5a0fcf5a91bbbaa2bdbab7b3212c286a509
Leonardo César <leo_cesa@hotmail.com>
2024-01-16 17:36:59 -0300

    chore: timestampToDate

--- src/utils/date/timestampToDate.ts
+++ src/utils/date/timestampToDate.ts
@@ -1,6 +1,7 @@
 import { Timestamp } from "firebase/firestore";
 
 export const timestampToDate = (timestamp: Timestamp) => {
-    const date = new Date(timestamp.seconds * 1000);
-    return date;
+    // const date = new Date(timestamp.seconds * 1000);
+    const date = timestamp.toDate()
+    return date; 
   };
