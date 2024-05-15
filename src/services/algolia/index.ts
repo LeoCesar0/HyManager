f11c84cc4cddc1c6265d4386efc444efc9f05719
Leonardo César <leo_cesa@hotmail.com>
2024-05-14 21:37:55 -0300

    feat: install algolia search

---
+++ src/services/algolia/index.ts
@@ -0,0 +1,11 @@
+import algoliasearch from "algoliasearch";
+
+// Connect and authenticate with your Algolia app
+export const ALGOLIA_CLIENT = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_API_ID || "",
+  process.env.NEXT_PUBLIC_ALGOLIA_WRITE_API_KEY || ""
+);
+
+export const ALGOLIA_INDEXES = {
+  CREDITORS: "hymanager",
+};
