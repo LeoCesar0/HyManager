c3f7a5005bb2c5286ee1c4d2e1fb277c912ccc1d
Leonardo César <leo_cesa@hotmail.com>
2024-06-12 10:56:22 -0300

    chore: test page

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,46 +1,45 @@
-
 import { Section, SectionContainer } from "@/components/Section/Section";
-import DashboardLayout from '@/layouts/DashboardLayout';
+import { Button } from "@/components/ui/button";
+import DashboardLayout from "@/layouts/DashboardLayout";
+import { FirebaseCollection } from "@/server/firebase";
+import { firebaseList } from "@/server/firebase/firebaseList";
+import { Transaction } from "@/server/models/Transaction/schema";
+import { createDocRef } from "@/server/utils/createDocRef";
+import { firebaseDB } from "@/services/firebase";
+import { writeBatch } from "firebase/firestore";
 import { ReactElement } from "react";
 
 const Test = () => {
+  const run = async () => {
+    // const result = await firebaseList<Transaction>({
+    //   collection: FirebaseCollection.transactions,
+    // });
+    // const newData = result.map((item) => ({
+    //   ...item,
+    //   absAmount: Math.abs(item.amount),
+    // }));
+    // console.log("newData.length", newData.length);
+    // const batch = writeBatch(firebaseDB);
+    // newData.forEach((item) => {
+    //   const docRef = createDocRef({
+    //     collection: FirebaseCollection.transactions,
+    //     id: item.id,
+    //   });
+    //   batch.set(docRef, item, { merge: true });
+    // });
+    // await batch.commit();
+    // console.log("Success");
+  };
+
   return (
     <>
       <SectionContainer>
         <Section>
-          <div className="table-container">
-            <table>
-              <thead>
-                <tr>
-                  <th>Id</th>
-                  <th>Amount</th>
-                  <th>Creditor</th>
-                  <th>Created at</th>
-                  <th>Updated at</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>id</td>
-                  <td>500</td>
-                  <td>Fulano</td>
-                  <td>today</td>
-                  <td>today</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <Button onClick={() => run()}>Run</Button>
         </Section>
       </SectionContainer>
     </>
   );
 };
 
-// Test.getLayout = (page: ReactElement) => (
-//   <DashboardLayout>{page}</DashboardLayout>
-// );
-
 export default Test;
-
-
-
